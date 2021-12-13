6dda1568917ccf5a381025724ee70cc737d919ef
ShakalakaB <yunjie989@gmail.com>
2021-12-14 00:45:52 +0800

    Thread exception handler

--- src/main/java/fun/aldora/thread/creation/ThreadCreation.java
+++ src/main/java/fun/aldora/thread/creation/ThreadCreation.java
@@ -6,13 +6,21 @@ public class ThreadCreation {
         Thread thread = new Thread(() -> {
             System.out.println("in thread: " + Thread.currentThread().getName());
             System.out.println("thread " + Thread.currentThread().getName() + ": priority is " + Thread.currentThread().getPriority());
+            throw new RuntimeException("intentional exception");
         });
 
-        thread.setName("thread " + threadName);
+        thread.setName(threadName);
         thread.setPriority(Thread.MAX_PRIORITY);
+        thread.setUncaughtExceptionHandler(new Thread.UncaughtExceptionHandler() {
 
-        System.out.println("in thread: " + Thread.currentThread().getName() + " before starting a new thread");
+            @Override
+            public void uncaughtException(Thread t, Throwable e) {
+                System.out.println("Thread: " + t.getName() + ", error: " + e.getMessage());
+            }
+        });
+
+        System.out.println("in thread: " + Thread.currentThread().getName() + ", before starting a new thread");
         thread.start();
-        System.out.println("in thread: " + Thread.currentThread().getName() + " after starting a new thread");
+        System.out.println("in thread: " + Thread.currentThread().getName() + ", after starting a new thread");
     }
 }
