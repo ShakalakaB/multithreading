892c3eea347770bcccdf29f42c8003fce34d9835
ShakalakaB <yunjie989@gmail.com>
2021-12-22 01:30:33 +0800

    CompletableFuture

---
+++ src/main/java/fun/aldora/thread/creation/CompletableFutureExample.java
@@ -0,0 +1,28 @@
+package fun.aldora.thread.creation;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+
+public class CompletableFutureExample {
+    public static void main(String[] args) {
+        Integer b = 1;
+        CompletableFuture<Integer> completableFuture = CompletableFuture.supplyAsync(() -> {
+            return 1 + b;
+        }).thenCompose(s -> {
+            return CompletableFuture.supplyAsync(() -> {
+                return s + 1;
+            });
+        }).thenCombine(CompletableFuture.supplyAsync(() -> {
+                    return 1;
+                }), (s1, s2) -> s1 + s2
+        ).thenApply(supplier -> {
+            return supplier + 1;
+        });
+        try {
+            System.out.println(completableFuture.get());
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
