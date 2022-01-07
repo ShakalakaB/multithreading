036061d833d895f79ef7a5f7ff3fb48f32573af9
ShakalakaB <yunjie989@gmail.com>
2022-01-07 15:57:14 +0800

    Use try/catch to handle executorService exception

--- java-multithreading/src/main/java/fun/aldora/multithreading/exception/HandleExecutorServiceException.java
+++ java-multithreading/src/main/java/fun/aldora/multithreading/exception/HandleExecutorServiceException.java
@@ -4,16 +4,24 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class HandleExecutorServiceException {
+    private static Logger logger = Logger.getLogger("HandleExecutorServiceException");
+
     public static void main(String[] args) {
         ExecutorService executorService = Executors.newFixedThreadPool(3);
 
         Callable<Integer> callableTask = () -> {
             System.out.println("inside " + Thread.currentThread().getName());
+            if (1 == 1) {
+                throw new RuntimeException("TEST");
+            }
             return 3;
         };
 
@@ -25,6 +33,15 @@ public class HandleExecutorServiceException {
             futureList.add(future);
         }
 
+        for (Future<Integer> future : futureList) {
+            try {
+                Integer result = future.get();
+            } catch (InterruptedException e) {
+                logger.log(Level.SEVERE, e.getMessage());
+            } catch (ExecutionException e) {
+                logger.log(Level.SEVERE, e.getMessage());
+            }
+        }
 
     }
 }
